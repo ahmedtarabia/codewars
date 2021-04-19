6674404e14d8d0ec4eb895e5bf97e15595c8526b
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-04-19 06:53:16 -0400

    heeey

---
+++ challenge-016.js
@@ -0,0 +1,18 @@
+const repeatNumbers = function(data) {
+  let value = " ";
+  for(i = 0; i<data.length; i++) {
+    for(j = 0; j<data[i][1]; j++) {
+      value += data[i][0].toString();
+    }
+    if(i !== data.length - 1) {
+      value += ",";
+    }
+  }
+  return value
+};
+
+console.log(repeatNumbers([[1, 10]]));
+console.log(repeatNumbers([[1, 2], [2, 3]]));
+console.log(repeatNumbers([[10, 4], [34, 6], [92, 2]]));
+
+
