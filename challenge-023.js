9d13124112fd2d1a6187becbb7c3aa877e2bd2fb
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-11-14 17:42:39 -0500

    chal

---
+++ challenge-023.js
@@ -0,0 +1,18 @@
+
+
+//PREP
+
+// Parameters: String with spaces
+// Return: String without spaces
+// Example: "Hi How are you" returned "HiHowareyou"
+// Pseudocode: Convert string into array using split then filter out spaces and then join it back. 
+
+// function removeSpaces (str) {
+//   return str.split("").filter(i => i !== " ").join("")
+// }
+
+// function removeSpaces (str) {
+//   return str.split(" ").join("")
+// }
+
+
